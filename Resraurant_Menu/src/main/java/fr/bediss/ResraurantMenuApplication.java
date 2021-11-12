88c29c32f35037640ff55ee19efad0962d3b901e
Bédiss NAHDI <nahdibediss@yahoo.fr>
2021-11-12 14:15:35 +0100

    Création Projet Spring boot : Restaurant Menu(s)

---
+++ Resraurant_Menu/src/main/java/fr/bediss/ResraurantMenuApplication.java
@@ -0,0 +1,13 @@
+package fr.bediss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResraurantMenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResraurantMenuApplication.class, args);
+	}
+
+}
---
+++ Resraurant_Menu/src/test/java/fr/bediss/ResraurantMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.bediss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResraurantMenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
